bc650f275a83412cdff7a7e276f17469e0b6072e
Johannes Schuba <johannes@herrlich-media.de>
2025-05-16 18:47:16 +0200

    Exercise 3

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
   return (
-    <div className="guess-results">
-      {guesses.map(({ word, id }) => (
-        <p key={id} className="guess">{word}</p>
-      ))}
-    </div>
+      <div className="guess-results">
+        {range(0, NUM_OF_GUESSES_ALLOWED).map((num) => (
+          <Guess key={guesses[num].id} guess={guesses[num]} />
+        ))}
+      </div>
   );
 }
 
