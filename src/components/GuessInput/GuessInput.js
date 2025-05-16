699c03e2ea7262dc02d654397aca63ae7026647e
Johannes Schuba <johannes@herrlich-media.de>
2025-05-16 17:21:28 +0200

    Exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(guess);
+    setGuess('');
+  }
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
